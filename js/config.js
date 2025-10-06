d5111576376498ed0f4f4ee506c931d13bd02355
MeenakshiRuthna <meenakshiruthna@gmail.com>
2025-10-06 17:31:26 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_M2TXlvNDw', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '222hkrkaibm5hs879h95t9lihl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_HbA0QY8c1', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2o0d1o3d18j8teqmhunk5nsg3h', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
